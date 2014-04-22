4b35e74535266a6161f7addb070efc0cf080c5e5
kaestel <kaestel@e-types.com>
2014-04-22 20:00:33 +0200

    merged

--- src/templates/pages/manifest.php
+++ src/templates/pages/manifest.php
@@ -2,26 +2,28 @@
 
 	<h1>Development manifest</h1>
 	<p>
-		parentNode has fomulated the following development guidelines, to challenge conventional
-		development where decisions are made to maximize profit and to ensure that creativity, quality 
-		and ambition is prioritized over spreadsheet logic.
+		In an effort to ensure that creativity, quality and ambition is prioritized over spreadsheet logic 
+		parentNode has fomulated the following development guidelines, challenging traditional
+		business models where the majority of decisions are made to maximize profit.
 	</p> 
 
 	<ul class="manifest">
 		<li>The primary objective is always the end user</li>
-		<li>Ignore cost of development in favour of making the right decisions</li>
-		<li>Anything can be done with patience and hard work</li>
+		<li>Find a secondary objective which is important to you personally</li>
+		<li>Ignore cost of development in favour of making the right long term decisions</li>
+		<li>Bring patience and expect hard work</li>
 		<li>Explore, philosophize, rationalize</li>
+		<li>Be uncompromizingly ambitious</li>
 		<li>If you think you have a good idea, build it three times</li>
-		<li>Be ambitious</li>
 		<li>Concentrate, focus and think outside your box</li>
 		<li>Get lost in details</li>
 		<li>Restructure, refine, reimplement</li>
-		<li>Any experience you gain counts</li>
+		<li>Gain experience through insane failure</li>
 		<li>Share everything</li>
-		<li>Be stuborn but don't be stupid</li>
+		<li>Feel free to curse</li>
+		<li>Be stuborn</li>
 		<li>Do it anyway</li>
-		<li>Make yourself proud</li>
+		<li>... and make yourself proud</li>
 	</ul>
 
 </div>

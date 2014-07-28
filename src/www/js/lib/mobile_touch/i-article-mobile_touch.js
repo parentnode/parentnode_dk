488de2918251ded3515c957f1bba4cada9089529
kaestel <kaestel@e-types.com>
2014-07-28 13:05:47 +0200

    updated janitor

--- src/www/js/lib/mobile_touch/i-article-mobile_touch.js
+++ src/www/js/lib/mobile_touch/i-article-mobile_touch.js
@@ -81,7 +81,7 @@ Util.Objects["articlelist"] = new function() {
 //			u.bug("load next function")
 		
 			if(this._next) {
-
+		
 				// receive previous items
 				this.response = function(response) {
 
@@ -140,7 +140,7 @@ Util.Objects["article"] = new function() {
 			if(image._id && image._format) {
 
 				// add image
-				image._image_src = "/images/" + image._id + "/" + (image._variant ? image._variant+"/" : "") + image.offsetWidth + "x." + image._format;
+				image._image_src = "/images/" + image._id + "/" + (image._variant ? image._variant+"/" : "") + "480x." + image._format;
 				image._image = u.ie(image, "img");
 				u.a.setOpacity(image, 0);
 				image.loaded = function(queue) {
@@ -156,43 +156,6 @@ Util.Objects["article"] = new function() {
 					u.a.setOpacity(this, 1);
 				}
 				u.preloader(image, [image._image_src]);
-				// apply full-width option
-				u.ce(image);
-				image.clicked = function() {
-					// go back to normal size
-					if(u.hc(this, "fullsize")) {
-						u.rc(this, "fullsize");
-						this._image.src = this._image_src;
-					}
-					// switch to fullsize
-					else {
-						// full size image, might exceed autoconversion limit
-						// test server response
-
-						u.ac(this, "fullsize");
-
-						// fullsize already defined and tested
-						if(this._fullsize_src) {
-							this._image.src = this._fullsize_src;
-						}
-						else {
-							this._fullsize_width = 1300;
-							this._fullsize_src = "/images/" + this._id + "/" + (this._variant ? this._variant+"/" : "") + this._fullsize_width + "x." + this._format;
-
-							// valid response - set new src
-							this.response = function() {
-								this._image.src = this._fullsize_src;
-							}
-							// 404 - reduce size and try again
-							this.responseError = function() {
-								this._fullsize_width = this._fullsize_width-200;
-								this._fullsize_src = "/images/" + this._id + "/" + (this._variant ? this._variant+"/" : "") + this._fullsize_width + "x." + this._format;
-								u.request(this, this._fullsize_src);
-							}
-							u.request(this, this._fullsize_src);
-						}
-					}
-				}
 
 			}
 		}
@@ -226,7 +189,7 @@ Util.Objects["article"] = new function() {
 						html += 'var map, marker;';
 						html += 'var initialize = function() {';
 						html += '	window._map_loaded = true;';
-						html += '	var mapOptions = {center: new google.maps.LatLng('+this.geo_latitude+', '+this.geo_longitude+'),zoom: 12};';
+						html += '	var mapOptions = {center: new google.maps.LatLng('+this.geo_latitude+', '+this.geo_longitude+'),zoom: 12, scrollwheel: false, draggable: false};';
 						html += '	map = new google.maps.Map(document.getElementById("map"),mapOptions);';
 						html += '	marker = new google.maps.Marker({position: new google.maps.LatLng('+this.geo_latitude+', '+this.geo_longitude+'), draggable:true});';
 						html += '	marker.setMap(map);';

18466cbf0730a5622435fe955fb755595c26d605
kaestel <kaestel@e-types.com>
2014-10-06 18:34:56 +0200

    Added type:page

--- src/templates/www.footer.php
+++ src/templates/www.footer.php
@@ -1,11 +1,27 @@
+<?
+$navigation = 0; //session()->value("navigation_main");
+// or get complete structure from system
+if(!$navigation) {
+
+	$NC = new Navigation();
+	$navigation = $NC->getNavigations(array("handle" => "main"));
+
+	session()->value("navigation_main", $navigation);
+
+}
+?>
 	</div>
 
 	<div id="navigation">
 		<ul class="navigation">
-			<li class="front"><a href="/">Frontpage</a></li>
+<?			foreach($navigation["nodes"] as $node): ?>
+			<li<?= $HTML->attribute("class", $node["classname"]) ?>><a href="<?= ($node["link"] ? $node["link"] : ("/page/".$node["sindex"])) ?>"<?= strpos($node["link"], "http://") === 0 ? $HTML->attribute("target", "_blank") : "" ?>><?= $node["name"] ?></a></li>
+<?			endforeach; ?>
+
+			<!--li class="front"><a href="/">Frontpage</a></li>
 			<li class="blog"><a href="/blog">bLog</a></li>
 			<li class="manifest"><a href="/manifest">Manifest</a></li>
-			<li class="about"><a href="/about">About</a></li>
+			<li class="about"><a href="/about">About</a></li-->
 		</ul>
 	</div>
 

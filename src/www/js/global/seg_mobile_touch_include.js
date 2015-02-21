02a4ff30070a81a322ca5fda5c9eb2d549c0f844
kaestel <kaestel@e-types.com>
2015-02-21 21:00:55 +0000

    Global css+js update

---
+++ src/www/js/global/seg_mobile_touch_include.js
@@ -0,0 +1,16 @@
+document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/v0_9/full/seg_mobile_touch.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
+document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/v0_9/src/u-googleanalytics.js"></script>');
+
+
+// document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-events-browser.js"></script>');
+// document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-dom.js"></script>');
+//
+// document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-form.js"></script>');
+// document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-form-builder.js"></script>');
+
+document.write('<script type="text/javascript" src="http://parentnode.local/js/lib/mobile_touch/i-page-mobile_touch.js"></script>');
+document.write('<script type="text/javascript" src="http://parentnode.local/js/lib/mobile_touch/i-article-mobile_touch.js"></script>');
+

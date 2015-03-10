852c5e4b56b5f8abc11badc0fe0c71010a3906c7
kaestel <kaestel@e-types.com>
2015-03-10 19:32:06 +0000

    Updated header and footer for new janitor

--- src/templates/janitor.footer.php
+++ src/templates/janitor.footer.php
@@ -6,15 +6,16 @@
 			<?= $HTML->link("Pages", "/janitor/admin/page/list", array("wrapper" => "li.page")) ?>
 
 			<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
-
-			<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
 			<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
+			<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
+
+			<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
 		</ul>
 	</div>
 
 	<div id="footer">
 		<ul class="servicenavigation">
-			<li class="copyright">Janitor, Manipulator, Modulator - parentNode - Copyright 2014</li>
+			<li class="copyright">Janitor, Manipulator, Modulator - parentNode - Copyright 2015</li>
 		</ul>
 	</div>
 </div>
--- src/templates/www.footer.php
+++ src/templates/www.footer.php
@@ -1,13 +1,11 @@
 <?
-$navigation = 0; //session()->value("navigation_main");
+$navigation = session()->value("navigation_main");
 // or get complete structure from system
 if(!$navigation) {
 
 	$NC = new Navigation();
 	$navigation = $NC->getNavigations(array("handle" => "main"));
-
 	session()->value("navigation_main", $navigation);
-
 }
 ?>
 	</div>
@@ -16,7 +14,7 @@ if(!$navigation) {
 		<ul class="navigation">
 <?		if($navigation):
 			foreach($navigation["nodes"] as $node): ?>
-			<li<?= $HTML->attribute("class", $node["classname"]) ?>><a href="<?= ($node["link"] ? $node["link"] : ("/page/".$node["sindex"])) ?>"<?= strpos($node["link"], "http://") === 0 ? $HTML->attribute("target", "_blank") : "" ?>><?= $node["name"] ?></a></li>
+			<?= $HTML->navigationLink($node); ?>
 <?			endforeach;
 	 	endif; ?>
 		</ul>

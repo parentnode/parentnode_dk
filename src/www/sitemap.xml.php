c96331a78c1a13c98435ff15ae14fb32d254d222
kaestel <martin@think.dk>
2016-03-12 20:09:55 +0000

    Sitemap update

--- src/www/sitemap.xml.php
+++ src/www/sitemap.xml.php
@@ -5,33 +5,47 @@ if(isset($read_access) && $read_access) {
 }
 
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
+$query = new Query();
+$IC = new Items();
 
 print '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
-		<loc>http://parentnode.dk/</loc>
+		<loc><?= SITE_URL ?>/</loc>
 		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/front.php")) ?></lastmod>
 		<changefreq>weekly</changefreq>
 		<priority>1</priority>
 	</url>
 	<url>
-		<loc>http://parentnode.dk/manifest</loc>
+		<loc><?= SITE_URL ?>/blog/</loc>
+		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/posts.php")) ?></lastmod>
+		<changefreq>daily</changefreq>
+		<priority>1</priority>
+	</url>
+<?
+// POST ITEMS
+$items = $IC->getItems(array("itemtype" => "post", "status" => 1)); 
+foreach($items as $item):
+?>
+	<url>
+		<loc><?= SITE_URL ?>/blog/<?= $item["sindex"] ?></loc>
+		<lastmod><?= date("Y-m-d", strtotime($item["modified_at"])) ?></lastmod>
+		<changefreq>daily</changefreq>
+		<priority>1</priority>
+	</url>
+<? endforeach; ?>
+	<url>
+		<loc><?= SITE_URL ?>/manifest</loc>
 		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/manifest.php")) ?></lastmod>
 		<changefreq>weekly</changefreq>
 		<priority>1</priority>
 	</url>
 	<url>
-		<loc>http://parentnode.dk/about</loc>
+		<loc><?= SITE_URL ?>/about</loc>
 		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/about.php")) ?></lastmod>
 		<changefreq>weekly</changefreq>
 		<priority>1</priority>
-	</url>
-	<url>
-		<loc>http://parentnode.dk/terms</loc>
-		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/terms.php")) ?></lastmod>
-		<changefreq>monthly</changefreq>
-		<priority>0.5</priority>
 	</url>
 </urlset>

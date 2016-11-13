af57338d3439833c40b45d2f6d2c5cffef7b566c
kaestel <martin@think.dk>
2016-11-13 19:26:52 +0000

    Enabled placeholders in global signup js

--- src/www/js/global/desktop/i-signup.js
+++ src/www/js/global/desktop/i-signup.js
@@ -1,14 +1,10 @@
 Util.Objects["signup"] = new function() {
 	this.init = function(scene) {
-		u.bug("scene init:" + u.nodeId(scene))
+//		u.bug("scene init:" + u.nodeId(scene))
 		
 
 		scene.resized = function() {
 //			u.bug("scene.resized:" + u.nodeId(this));
-
-
-			// refresh dom
-			//this.offsetHeight;
 		}
 
 		scene.scrolled = function() {
@@ -18,11 +14,22 @@ Util.Objects["signup"] = new function() {
 		scene.ready = function() {
 //			u.bug("scene.ready:" + u.nodeId(this));
 
-			this._form = u.qs("form", this);
-			u.f.init(this._form);
-
-
 			page.cN.scene = this;
+
+			var signup_form = u.qs("form.signup", this);
+			var place_holder = u.qs("div.articlebody .placeholder.signup", this);
+
+			if(signup_form && place_holder) {
+				place_holder.parentNode.replaceChild(signup_form, place_holder);
+			}
+
+			if(signup_form) {
+				u.f.init(signup_form);
+			}
+
+			// accept cookies?
+			page.acceptCookies();
+
 			page.resized();
 		}
 

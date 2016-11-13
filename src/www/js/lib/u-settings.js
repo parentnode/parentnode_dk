1e29ccb57abe55d2b003d7fc089635d46da2acf7
kaestel <martin@think.dk>
2016-11-13 14:49:51 +0000

    Added sharing info

--- src/www/js/lib/u-settings.js
+++ src/www/js/lib/u-settings.js
@@ -9,6 +9,9 @@ u.ga_domain = 'parentnode.dk';
 // dynamic text
 u.txt = {};
 u.txt["share"] = "Share";
+u.txt["share-info-headline"] = "(How do I share?)";
+u.txt["share-info-txt"] = "We have not includered social media plugins on this site, because they are frequently abused to collect data about you. Also we don't want to promote some channels over others. Instead, just copy the link and share it wherever you find relevant.";
+u.txt["share-info-ok"] = "OK";
 
 u.txt["readstate-not_read"] = "Click to mark as read";
 u.txt["readstate-read"] = "Read";

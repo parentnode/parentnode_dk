ba54d4b1f3240601aa447fa1366281185a5ea8c5
Martin Kæstel Nielsen <martin@think.dk>
2017-01-20 22:58:38 +0100

    first post extension

--- src/www/js/seg_desktop.js
+++ src/www/js/seg_desktop.js
@@ -1,6 +1,6 @@
 /*
-Manipulator v0.9.1 Copyright 2016 http://manipulator.parentnode.dk
-js-merged @ 2017-01-20 18:14:49
+parentNode, Copyright 2017, https://.parentnode.dk
+js-merged @ 2017-01-20 22:55:09
 */
 
 /*seg_desktop_include.js*/
@@ -8021,3 +8021,31 @@ u.addNextArrow = function(node) {
 	});
 }
 
+
+/*i-post-customs.js*/
+Util.Objects["ubuntuSetupPost"] = new function() {
+	this.init = function(post) {
+		var extension_start = u.qs(".extension_start", post);
+		var form = u.f.addForm(post, {"class":"labelstyle:inject"});
+		var extension_text = u.ae(form, "p", {"html":"To make the following commands C/P ready, you can enter your information here:"});
+		var fieldset = u.f.addFieldset(form);
+		u.f.addField(fieldset, {"label":"IP address", "name":"ip", "class":"ip_master"});
+		u.f.addField(fieldset, {"label":"Username", "name":"username", "class":"username_master"});
+		u.f.init(form);
+		form.fields["ip"].ips = u.qsa("span.ip", post);
+		form.fields["ip"].updated = function() {
+			var i, ip;
+			for(i = 0; ip = this.ips[i]; i++) {
+				ip.innerHTML = this.val();
+			}
+		}
+		form.fields["username"].usernames = u.qsa("span.username", post);
+		form.fields["username"].updated = function() {
+			var i, username;
+			for(i = 0; username = this.usernames[i]; i++) {
+				username.innerHTML = this.val();
+			}
+		}
+		extension_start.parentNode.insertBefore(form, extension_start);
+	}
+}
--- src/www/js/seg_tablet.js
+++ src/www/js/seg_tablet.js
@@ -1,6 +1,6 @@
 /*
-Manipulator v0.9.1 Copyright 2016 http://manipulator.parentnode.dk
-js-merged @ 2017-01-20 18:14:49
+parentNode, Copyright 2017, https://.parentnode.dk
+js-merged @ 2017-01-20 22:55:10
 */
 
 /*seg_tablet_include.js*/

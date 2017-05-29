4fbacc471fa8de1022ef2ae6985b9262f21fd70d
Martin Kæstel Nielsen <martin@think.dk>
2017-05-29 23:40:08 +0200

    made dynamic vars in posts more flexible

--- src/www/js/lib/desktop/i-post-customs.js
+++ src/www/js/lib/desktop/i-post-customs.js
@@ -32,3 +32,44 @@ Util.Objects["ubuntuSetupPost"] = new function() {
 
 	}
 }
+
+Util.Objects["dynamicVariablesPost"] = new function() {
+	this.init = function(post) {
+
+		var extension_start = u.qs(".extension_start", post);
+
+		var form = u.f.addForm(post, {"class":"labelstyle:inject dynamicVariablesPost"});
+		var extension_text = u.ae(form, "p", {"html":"To make the following commands C/P ready, enter your information here:"});
+
+		var fieldset = u.f.addFieldset(form);
+
+		var var_names = {};
+		var dyn_vars = u.qsa("span.dynvar");
+		var i, dyn_var;
+		for(i = 0; dyn_var = dyn_vars[i]; i++) {
+			dyn_var.var_name = dyn_var.className.replace(/(dynvar|[ ])/g, "");
+			if(!var_names[dyn_var.var_name]) {
+				var_names[dyn_var.var_name] = u.f.addField(fieldset, {"label":dyn_var.innerHTML, "name":dyn_var.var_name, "class":dyn_var.var_name+"_master"});
+
+			}
+			
+		}
+
+
+		u.f.init(form);
+
+		for(i = 0; dyn_var = dyn_vars[i]; i++) {
+
+			form.fields[dyn_var.var_name].placeholders = u.qsa("span.dynvar."+dyn_var.var_name, post);
+			form.fields[dyn_var.var_name].updated = function() {
+				var i, placeholder;
+				for(i = 0; placeholder = this.placeholders[i]; i++) {
+					placeholder.innerHTML = this.val();
+				}
+			}
+		}
+
+		extension_start.parentNode.insertBefore(form, extension_start);
+
+	}
+}

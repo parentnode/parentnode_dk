23760796c87feea27a1da6f80d8b0691868d3aad
Søren Lyhne <soren@parentnode.dk>
2018-10-03 11:16:55 +0200

    update includes

--- theme/www/js/global/seg_desktop_include.js
+++ theme/www/js/global/seg_desktop_include.js
@@ -1,7 +1,7 @@
 document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/v0_9_2/merged/seg_desktop.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/u-basics.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/u-basics.js"></script>');
 document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/v0_9_2/src/u-googleanalytics.js"></script>');
 
 document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-cookie.js"></script>');
@@ -19,11 +19,11 @@ document.write('<script type="text/javascript" src="http://parentnode.local/js/m
 document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/v0_9_2/src/u-date.js"></script>');
 
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-page.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-front.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-scene.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-login.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-signup.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-newsletter.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-article.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-todolist.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-page.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-front.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-scene.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-login.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-signup.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-newsletter.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-article.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/src/js/desktop/i-todolist.js"></script>');

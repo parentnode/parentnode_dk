be44475f1444ab1515d9da04fd16db367fc7dcd5
threepwoodd <scope.dk@gmail.com>
2019-03-26 18:38:20 +0100

    Update includes

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,14 +1,16 @@
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/seg_desktop_include.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-article_mini_list.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-article_mini_list.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-comments.js"></script>');
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-comments.js"></script>');
-
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/u-geolocation.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/u-sharing.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/u-checkmark.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/u-expandarrow.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/u-geolocation.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/u-sharing.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/u-checkmark.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/u-expandarrow.js"></script>');
 
 
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-post-customs.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-demos.js"></script>');
+
+

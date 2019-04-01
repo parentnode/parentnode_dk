c2dfbc426080d0818b8391916bd114130091cfb1
threepwoodd <scope.dk@gmail.com>
2019-04-01 15:33:46 +0200

    Update includes

--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -1,8 +1,10 @@
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_smartphone.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartpone/i-login.js"></script>');
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartpone/i-signup.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-login.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-signup.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-verify.js"></script>');
+
 
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/u-geolocation.js"></script>');
 

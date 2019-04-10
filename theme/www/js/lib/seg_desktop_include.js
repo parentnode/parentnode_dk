39f73a9ce5609e3c028aa3e7090285da03847e6e
threepwoodd <scope.dk@gmail.com>
2019-04-10 12:54:23 +0200

    Update includes

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,9 +1,9 @@
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_desktop.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-events-movements.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/beta-u-animation-to.js"></script>');
 
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-dynamic-variables-post.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-article_mini_list.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-comments.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-login.js"></script>');
@@ -17,7 +17,6 @@ document.write('<script type="text/javascript" src="/assets/parentnode-skin-defa
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/u-expandarrow.js"></script>');
 
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-post-customs.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-demos.js"></script>');
 
 
--- theme/www/js/lib/seg_tablet_include.js
+++ theme/www/js/lib/seg_tablet_include.js
@@ -1,6 +1,7 @@
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_tablet.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-dynamic-variables-post.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-article_mini_list.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-comments.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-login.js"></script>');

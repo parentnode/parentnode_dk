8c0aa4bdbde8ef55f3b17c4920c52f8a48cb0009
kaestel <martin@think.dk>
2023-06-13 13:00:19 +0200

    GA4

--- theme/www/js/lib/u-settings.js
+++ theme/www/js/lib/u-settings.js
@@ -2,6 +2,6 @@ u.site_name = "parentNode";
 u.terms_version = "terms_v1";
 u.github_fork = {"url":"https://github.com/parentnode", "text":"parentNode on GitHub"};
 
-u.ga_account = 'UA-49720985-1';
+u.ga_account = 'G-10DCX2DD2C';
 u.ga_domain = 'parentnode.dk';
 

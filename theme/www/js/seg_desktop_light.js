d54d64b2a284ae147079c9de116a77645c5d1859
kaestel <martin@think.dk>
2024-01-24 12:03:42 +0100

    fix color

--- theme/www/js/seg_desktop_light.js
+++ theme/www/js/seg_desktop_light.js
@@ -1,6 +1,6 @@
 /*
 parentNode, Copyright 2008-2023, https://manipulator.parentnode.dk
-asset-builder @ 2024-01-24 12:01:51
+asset-builder @ 2024-01-24 12:03:33
 */
 
 /*seg_desktop_light_include.js*/
